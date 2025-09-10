2d2fefe987ad5116f92a3a79045c13c3e277c680
Isaac Ferreira <isaac.ferreira@insidesistemas.com.br>
2025-09-10 09:55:49 -0300

    refactor: remove style

--- actionplanner/actionplanner-frontend/src/app/modules/authentication/login/login.component.ts
+++ actionplanner/actionplanner-frontend/src/app/modules/authentication/login/login.component.ts
@@ -3,9 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  templateUrl: './login.component.html'
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}

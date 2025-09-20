970ef6945c7d12de736690e75ded81a80a3a8cd2
mayhmemo <mayhm.social@proton.me>
2025-09-20 02:14:12 -0300

    refactor: add global validation pipe

--- actionplanner/actionplanner-backend/src/main.ts
+++ actionplanner/actionplanner-backend/src/main.ts
@@ -20,6 +20,14 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

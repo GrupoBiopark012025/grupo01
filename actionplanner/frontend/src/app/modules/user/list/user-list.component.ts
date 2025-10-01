cbf29843d2040f79e2b417200be10390ab4929d8
mayhmemo <mayhm.social@proton.me>
2025-10-01 19:45:06 -0300

    feat: create basic skeleton for user list

--- actionplanner/frontend/src/app/modules/user/list/user-list.component.ts
+++ actionplanner/frontend/src/app/modules/user/list/user-list.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { map, Observable, shareReplay, switchMap } from "rxjs";
+import { GetUserDto, GetUserQuery } from "@data/user/dtos";
+import { UserDataService } from "@data/user/user-data.service";
+import { takeUntilDestroyed, toObservable } from "@angular/core/rxjs-interop";
+import { AsyncPipe } from "@angular/common";
+import { ApiPaginatedList } from "@data/common/dtos";
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
-  templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  imports: [
+    AsyncPipe
+  ],
+  templateUrl: './user-list.component.html'
 })
 export class UserListComponent {
+  private _query = signal<GetUserQuery>(new GetUserQuery());
 
+  private readonly userDataService = inject(UserDataService);
+  private readonly destroyRef = inject(DestroyRef);
+
+  users$: Observable<ApiPaginatedList<GetUserDto>> = toObservable(this._query)
+    .pipe(
+      switchMap(() => this.userDataService.getUsers(this._query())),
+      shareReplay(1),
+      takeUntilDestroyed(this.destroyRef)
+    );
+
+  alterarQuery(changes: Partial<UserDataService> = {}): void {
+    this._query.update((prev) => ({ ...prev!, ...changes }));
+  }
 }

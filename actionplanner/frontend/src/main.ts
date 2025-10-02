d6c555a94027e4b2f8aa6f30e94f2c23fd6a6db5
mayhmemo <mayhm.social@proton.me>
2025-10-01 21:48:55 -0300

    refactor: apply locale data to application

--- actionplanner/frontend/src/main.ts
+++ actionplanner/frontend/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from "@angular/common";
+import localeBr from '@angular/common/locales/pt';
+import localeBrExtra from '@angular/common/locales/extra/pt';
+
+registerLocaleData(localeBr, 'pt', localeBrExtra);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));

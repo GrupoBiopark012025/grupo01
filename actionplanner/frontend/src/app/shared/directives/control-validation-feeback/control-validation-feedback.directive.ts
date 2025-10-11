aa60d9671a3f73fd3ec92deddfc545884504b916
mayhmemo <mayhm.social@proton.me>
2025-10-11 15:27:04 -0300

    feat: create control validation feedback directive

---
+++ actionplanner/frontend/src/app/shared/directives/control-validation-feeback/control-validation-feedback.directive.ts
@@ -0,0 +1,26 @@
+import { computed, Directive, inject, input } from '@angular/core';
+import { FormControl } from "@angular/forms";
+import { ValidationService } from "@shared/services/validation/validation.service";
+
+@Directive({
+  selector: '[controlValidationFeedback]',
+  exportAs: 'controlValidationFeedback'
+})
+export class ControlValidationFeedbackDirective {
+  controlValidationFeedback = input<FormControl>();
+
+  private readonly validationService = inject(ValidationService);
+
+  get errorMessage() {
+    const control = this.controlValidationFeedback();
+
+    if (!control || !control.errors || !(control.dirty || control.touched)) { return ''; }
+
+    for (const propertyName in control.errors) {
+      if (!control.errors.hasOwnProperty(propertyName)) { continue; }
+      return this.validationService.getValidatorErrorMessage(propertyName, control.errors[propertyName]);
+    }
+
+    return '';
+  };
+}

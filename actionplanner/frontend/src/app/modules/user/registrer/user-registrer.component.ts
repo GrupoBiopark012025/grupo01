8c8a680cd0d50bca8d74109df8580e278a2b787e
mayhmemo <mayhm.social@proton.me>
2025-10-14 19:31:58 -0300

    refactor: aplica select genérico

--- actionplanner/frontend/src/app/modules/user/registrer/user-registrer.component.ts
+++ actionplanner/frontend/src/app/modules/user/registrer/user-registrer.component.ts
@@ -1,39 +1,29 @@
 import { Component, computed, DestroyRef, inject, signal } from '@angular/core';
 import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
-import { userAccessLevelByString, UserAccessLevelEnum, UserStatusEnum } from "@data/user/dtos";
+import { userAccessLevelByString, UserAccessLevelEnum, userAccessLevelOptions, UserStatusEnum } from "@data/user/dtos";
 import { UserDataService } from "@data/user/user-data.service";
 import { take } from "rxjs";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { ValidationService } from "@shared/services/validation/validation.service";
 import { Router } from "@angular/router";
 import { toast } from "ngx-sonner";
-import {
-  ZardFormControlComponent,
-  ZardFormFieldComponent,
-  ZardFormLabelComponent
-} from "@shared/components/zardui/form/form.component";
 import { PageHeaderComponent } from "@shared/components/base/page-header/page-header.component";
 import { ZardButtonComponent } from "@shared/components/zardui/button/button.component";
 import { BackOrNavigateToDirective } from "@shared/directives/back-or-navigate-to/back-or-navigate-to.directive";
-import { ZardSelectComponent } from "@shared/components/zardui/select/select.component";
-import { ZardSelectItemComponent } from "@shared/components/zardui/select/select-item.component";
 import { TextInputComponent } from "@shared/components/base/form-components/text-input/text-input.component";
 import { CheckboxComponent } from "@shared/components/base/form-components/checkbox/checkbox.component";
+import { SelectComponent } from "@shared/components/base/form-components/select/select.component";
 
 @Component({
   selector: 'app-user-registrer',
   imports: [
     ReactiveFormsModule,
-    ZardFormFieldComponent,
-    ZardFormControlComponent,
-    ZardFormLabelComponent,
     PageHeaderComponent,
     ZardButtonComponent,
     BackOrNavigateToDirective,
-    ZardSelectComponent,
-    ZardSelectItemComponent,
     TextInputComponent,
-    CheckboxComponent
+    CheckboxComponent,
+    SelectComponent
   ],
   templateUrl: './user-registrer.component.html'
 })
@@ -85,22 +75,6 @@ export class UserRegistrerComponent {
       });
   }
 
-  isFieldInvalid(fieldName: keyof FormData): boolean {
-    const field = this.form.controls[fieldName];
-    return !!(field?.invalid && (field?.dirty || field?.touched));
-  }
-
-  getEmailError(): string {
-    const email = this.form.get('email');
-    if (email?.hasError('required')) {
-      return 'Email é obrigatório';
-    }
-    if (email?.hasError('email')) {
-      return 'Por favor, insira um email válido';
-    }
-    return '';
-  }
-
   onAccessLevelChange(value: string) {
     const accessLevel = userAccessLevelByString[value];
 
@@ -108,6 +82,7 @@ export class UserRegistrerComponent {
   }
 
   protected readonly UserAccessLevelEnum = UserAccessLevelEnum;
+  protected readonly userAccessLevelOptions = userAccessLevelOptions;
 }
 
 interface FormData {

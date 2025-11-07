d884f474b2d9a53f18422ad79d771bafb47a1550
mayhmemo <mayhm.social@proton.me>
2025-11-06 22:11:06 -0300

    feat: apply sector routes

--- actionplanner/frontend/src/app/modules/sector/sector.routes.ts
+++ actionplanner/frontend/src/app/modules/sector/sector.routes.ts
@@ -10,6 +10,16 @@ export const SectorRoutes: Routes = [
         path: '',
         title: 'Setores - ActionPlanner',
         loadComponent: () => import('./list/sector-list.component').then((c) => c.SectorListComponent)
+      },
+      {
+        path: 'register',
+        title: 'Novo Setor - ActionPlanner',
+        loadComponent: () => import('./form/sector-form.component').then((c) => c.SectorFormComponent)
+      },
+      {
+        path: ':id/edit',
+        title: 'Editar Setor - ActionPlanner',
+        loadComponent: () => import('./form/sector-form.component').then((c) => c.SectorFormComponent)
       }
     ]
   }

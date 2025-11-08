c24efc4256ab04c0d3db62e41b137b91b848641c
mayhmemo <mayhm.social@proton.me>
2025-11-07 21:35:27 -0300

    chore: update badge, button and table

--- actionplanner/frontend/src/app/shared/components/zardui/button/button.component.ts
+++ actionplanner/frontend/src/app/shared/components/zardui/button/button.component.ts
@@ -1,34 +1,28 @@
-import { ClassValue } from 'clsx';
-
 import { ChangeDetectionStrategy, Component, computed, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
+import type { ClassValue } from 'clsx';
 
-import { mergeClasses, transform } from '@shared/utils/zardui/merge-classes';
 import { buttonVariants, ZardButtonVariants } from './button.variants';
+import { mergeClasses, transform } from '@shared/utils/zardui/merge-classes';
+import { ZardIconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'z-button, button[z-button], a[z-button]',
   exportAs: 'zButton',
   standalone: true,
+  imports: [ZardIconComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   template: `
     @if (zLoading()) {
-      <svg class="animate-spin w-4 h-4" viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" fill="none"></circle>
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-      </svg>
+      <z-icon zType="loader-circle" class="animate-spin" />
     }
-
     <ng-content></ng-content>
   `,
   host: {
     '[class]': 'classes()',
-    '[attr.disabled]': 'disabled() ? "" : null',
   },
 })
 export class ZardButtonComponent {
-  private readonly elementRef = inject(ElementRef);
-
   readonly zType = input<ZardButtonVariants['zType']>('default');
   readonly zSize = input<ZardButtonVariants['zSize']>('default');
   readonly zShape = input<ZardButtonVariants['zShape']>('default');
@@ -37,9 +31,17 @@ export class ZardButtonComponent {
 
   readonly zFull = input(false, { transform });
   readonly zLoading = input(false, { transform });
-  readonly disabled = input(false, { transform });
 
   protected readonly classes = computed(() =>
-    mergeClasses(buttonVariants({ zType: this.zType(), zSize: this.zSize(), zShape: this.zShape(), zFull: this.zFull(), zLoading: this.zLoading() }), this.class()),
+    mergeClasses(
+      buttonVariants({
+        zType: this.zType(),
+        zSize: this.zSize(),
+        zShape: this.zShape(),
+        zFull: this.zFull(),
+        zLoading: this.zLoading(),
+      }),
+      this.class(),
+    ),
   );
 }
